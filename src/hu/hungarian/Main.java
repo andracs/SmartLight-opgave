05477afcbc55f367d3a2ea43e7d9a065311fd469
Dalphs <42994000+Dalphs@users.noreply.github.com>
2018-10-25 10:28:56 +0200

    Finished excercise

--- src/hu/hungarian/Main.java
+++ src/hu/hungarian/Main.java
@@ -7,7 +7,6 @@ import java.lang.reflect.Field;
 public class Main {
 
     public static void main(String[] args) {
-	    // write your code here
         SmartLight s = new SmartLight();
         System.out.println(s.toString());
     }
--- src/hu/hungarian/SmartLight.java
+++ src/hu/hungarian/SmartLight.java
@@ -3,7 +3,46 @@ package hu.hungarian;
 import javafx.scene.paint.Color;
 
 public class SmartLight {
+    private short lightPercent = 0;
+    private Color color = Color.WHITESMOKE;
 
+    SmartLight(){
+    }
 
+    public void turnUp(){
+        lightPercent += 20;
+        if(lightPercent > 100)
+            lightPercent = 100;
+    }
 
+    public void turnDown(){
+        lightPercent -= 20;
+        if(lightPercent < 0)
+            lightPercent = 0;
+    }
+
+    public String toString(){
+        return "The light is at " + lightPercent + "% and the color is " + color;
+    }
+
+    public short getLightPercent() {
+        return lightPercent;
+    }
+
+    public void setLightPercent(short lightPercent) {
+        this.lightPercent = lightPercent;
+        if(lightPercent < 0)
+            this.lightPercent = 0;
+        else if(lightPercent > 100)
+            this.lightPercent = 100;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        if(color != Color.BLACK)
+            this.color = color;
+    }
 }
