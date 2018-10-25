89529f5ca47bf6b21f52d012203eb670f28d703e
Dalphs <42994000+Dalphs@users.noreply.github.com>
2018-10-25 10:50:07 +0200

    TECHNO LIGHTING

--- src/hu/hungarian/Main.java
+++ src/hu/hungarian/Main.java
@@ -9,5 +9,6 @@ public class Main {
     public static void main(String[] args) {
         SmartLight s = new SmartLight();
         System.out.println(s.toString());
+        s.operation();
     }
 }
